0284ee86b298a34a8f93706fc76a3b891ce8098e
Dwight Ledet <35194827+daledet@users.noreply.github.com>
2020-01-24 08:55:18 +0100

    Add files via upload

--- welldata/surveys/views.py
+++ welldata/surveys/views.py
@@ -1,10 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Survey
-# Create your views here.
 
 
 def index(request):
     surveys = Survey.objects.all()
-    output = ', '.join([s.survey for s in surveys])
-    return HttpResponse(output)
+    return render(request, 'surveys/index.html', {'surveys': surveys})
